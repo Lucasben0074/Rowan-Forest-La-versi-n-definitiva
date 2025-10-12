4b98997d2225fb97db3013e50f976454608afc0d
Lucas Benitez <lucasben0074@gmail.com>
2025-10-12 11:29:33 -0300

    Actualizo scripts del proyectil y del jugador en LVL1

--- Assets/Scripts/LogicGame/menuManager.cs
+++ Assets/Scripts/LogicGame/menuManager.cs
@@ -6,7 +6,7 @@ public class menuManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void StartGame()
     {
-        SceneManager.LoadScene("LevelDesign");
+        SceneManager.LoadScene("LEVEL1");
     }
 
     public void QuitGame()
